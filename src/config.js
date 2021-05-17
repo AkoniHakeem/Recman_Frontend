ec7308696d4ac9c59f8ddc778bb0bf7d201e7c68
akoni mayowa <akozymayor@gmail.com>
2021-05-17 02:53:25 +0100

    adjusted config

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const environments = {
         backendApiPath: "/api/v1"
     },
     "production": {
-        backendUrl: process.env.BACKENDAPIPATH,
-        backendApiPath: process.env.BACKENDAPIPATH || "/api/v1"
+        backendUrl: process.env["RECMAN_BACKEND_URL"],
+        backendApiPath: process.env["RECMAN_BACKEND_API_PATH"] || "/api/v1"
     }
 }
 process.env.NODE_ENV || console.log("NODE_ENV is not defined") 
