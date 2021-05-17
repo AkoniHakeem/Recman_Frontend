837ac51318a340b20467470778221dd644612fd8
akoni mayowa <akozymayor@gmail.com>
2021-05-17 03:40:59 +0100

    updated the config variables

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ const environments = {
         backendApiPath: "/api/v1"
     },
     "production": {
-        backendUrl: process.env["RECMAN_BACKEND_URL"],
-        backendApiPath: process.env["RECMAN_BACKEND_API_PATH"] || "/api/v1"
+        backendUrl: "https://recman-dlwjq.ondigitalocean.app",
+        backendApiPath: "/api/v1"
     }
 }
-process.env.NODE_ENV || console.log("NODE_ENV is not defined") 
+
 const envName = process.env.NODE_ENV.trim()
 const currentEnvironment = environments[`${process.env.NODE_ENV.toLowerCase()}`] || environments.development;
 
