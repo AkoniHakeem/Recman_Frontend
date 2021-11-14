96dba5dbc1973a9bdf311c87839a587d16320de4
AkoniHakeem <akozymayor@gmail.com>
2021-11-14 05:16:15 -0500

    updated backend url in config file

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const environments = {
         backendApiPath: "/api/v1"
     },
     "production": {
-        backendUrl: "https://recman-dlwjq.ondigitalocean.app",
+        backendUrl: "https://port-3001-my-first-container-akozymayor288218.preview.codeanywhere.com",
         backendApiPath: "/api/v1"
     }
 }
